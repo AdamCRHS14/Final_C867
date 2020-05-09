2b32fc3dae6b264e463cbfb7b20f300048bee3ff
curiouscoding22 <lambertbe22@gmail.com>
2020-05-09 14:56:16 -0400

    Inserted dummy information in main

--- Main.cpp
+++ Main.cpp
@@ -16,7 +16,7 @@ int main() {
 		"A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK",
 		"A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE",
 		"A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY",
-		"A5,Blake,Lambert,blamb52@wgu.edu,29,15,25,22,SOFTWARE"
+		"A5,Dummy,Dummy,Dummy,Dummy,Dummy,Dummy,Dummy,SOFTWARE"
 	};
 
 	int numStudents = 5;
@@ -25,7 +25,7 @@ int main() {
 	
 
 	cout << "C867 Scrpiting and Programming Applications" << endl;
-	cout << "Written in C++ by Blake Lambert, Student ID: #001337333" << endl;
+	cout << "Written in C++ by Blake Lambert, Student ID: " << endl;
 
 	for (int i = 0; i < numStudents; i++) {
 		classRoster->parseAndAdd(studentData[i]);
